14c0baf7842cecb2ceefc1dae1c7a21fc6994e2d
Stanislav Pirx <stanislav.pirx@gmail.com>
2023-06-24 14:28:16 +0300

    first commit

---
+++ src/bin/minimal.rs
@@ -0,0 +1,94 @@
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+
+use drawer_robot as _; // global logger + panicking-behavior + memory layout
+
+#[rtic::app(
+    device = stm32f1xx_hal::pac,
+    peripherals = true,
+    // You can usually find the names of the interrupt vectors in the some_hal::pac::interrupt enum.
+    dispatchers = [EXTI0, EXTI1, EXTI2, EXTI3, EXTI4]
+    // dispatchers = [PVD, WWDG, RTC, SPI1]
+)]
+mod app {
+
+    use rtic_monotonics::systick::*;
+    use stm32f1xx_hal::{pac, prelude::*, rcc, timer::Timer};
+
+    // Shared resources go here
+    #[shared]
+    struct Shared {
+        // TODO: Add resources
+    }
+
+    // Local resources go here
+    #[local]
+    struct Local {
+        led: stm32f1xx_hal::gpio::Pin<'C', 1, stm32f1xx_hal::gpio::Output>,
+    }
+
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local) {
+        defmt::info!("init");
+
+        // Take ownership over the raw flash and rcc devices and convert them into the corresponding
+        // HAL structs
+        let mut flash: stm32f1xx_hal::flash::Parts = cx.device.FLASH.constrain();
+        let mut rcc: stm32f1xx_hal::rcc::Rcc = cx.device.RCC.constrain();
+
+        // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
+        // `clocks`
+        // with those values one second equals 'delay_cycles: u32 = 72_000_000'
+        let clocks = rcc
+            .cfgr
+            .use_hse(8.MHz())
+            .sysclk(72.MHz())
+            .pclk1(36.MHz())
+            .pclk2(72.MHz())
+            .freeze(&mut flash.acr);
+
+        if !clocks.usbclk_valid() {
+            panic!("Clock parameter values are wrong!");
+        }
+
+        // Acquire the GPIOC peripheral
+        let mut gpioc: stm32f1xx_hal::gpio::gpioc::Parts = cx.device.GPIOC.split();
+        let mut gpiod: stm32f1xx_hal::gpio::gpiod::Parts = cx.device.GPIOD.split();
+        let led = gpioc.pc1.into_push_pull_output(&mut gpioc.crl);
+
+        let systick_mono_token = rtic_monotonics::create_systick_token!();
+        Systick::start(cx.core.SYST, 72_000_000, systick_mono_token); // default STM32F303 clock-rate is 36MHz
+
+        task1::spawn().ok();
+
+        (
+            Shared {
+                // Initialization of shared resources go here
+            },
+            Local {
+                led, // Initialization of local resources go here
+            },
+        )
+    }
+
+    // Optional idle, can be removed if not needed.
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        defmt::debug!("idle");
+
+        loop {
+            cortex_m::asm::nop();
+        }
+    }
+
+    // TODO: Add tasks
+    #[task(priority = 1, local = [led])]
+    async fn task1(cx: task1::Context) {
+        loop {
+            defmt::debug!("Hello from blink task!");
+            cx.local.led.toggle();
+            Systick::delay(1000.millis()).await;
+        }
+    }
+}
