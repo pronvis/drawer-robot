3cc9dee46454f91403c0b3be0b42d4f51e6e81c9
Stanislav Pirx <stanislav.pirx@gmail.com>
2023-07-11 12:38:35 +0300

    nothing important
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- src/my_stepper/mod.rs
+++ src/my_stepper/mod.rs
@@ -1,7 +1,9 @@
 #[derive(Clone)]
 pub struct MyStepperState {
     //Regulate Speed with that value.
-    //min value that works as fast as possible: 200 micros
+    //min value that works as fast as possible:
+    // 400 micros for full step
+    // 200 micros for 1/2 and smaller steps
     //max value that works smooth: 1500 micros
     pub micros_between_steps: u32,
 
@@ -9,3 +11,5 @@ pub struct MyStepperState {
     //max value that works smooth: 900 micros
     pub micros_pulse_duration: u32,
 }
+
+// My choice: 1/2 step'a
