35f02d02d16c1eeab2bef604f0ff6b6e3cd27bf7
Stanislav Pirx <stanislav.pirx@gmail.com>
2024-06-25 11:37:01 +0300

    nothing important
    Signed-off-by: Stanislav Pirx <stanislav.pirx@gmail.com>

--- crates/experiments/src/bin/software_serial_read_stepper.rs
+++ crates/experiments/src/bin/software_serial_read_stepper.rs
@@ -53,6 +53,7 @@ mod app {
         tmc2209_communicator_timer: Counter<stm32f1xx_hal::pac::TIM2, TMC2209COMMUNICATOR_CLOCK_FREQ>,
         tmc2209_msg_sender_1: Tmc2209RequestProducer,
         tmc2209_msg_sender_2: Tmc2209RequestProducer,
+        tmc2209_rsp_receiver: Tmc2209ResponseConsumer,
 
         read_timer: Counter<stm32f1xx_hal::pac::TIM3, READ_CLOCK_FREQ>,
         write_timer: Counter<stm32f1xx_hal::pac::TIM4, WRITE_CLOCK_FREQ>,
@@ -87,35 +88,36 @@ mod app {
 
         // Acquire the GPIOC peripheral
         let mut gpioc: stm32f1xx_hal::gpio::gpioc::Parts = cx.device.GPIOC.split();
+        let mut en = gpioc.pc7.into_push_pull_output(&mut gpioc.crl);
+        en.set_low();
+        let x_stepper_uart_pin = gpioc.pc10.into_dynamic(&mut gpioc.crh);
 
         let mut read_timer = robot_core::get_counter(cx.device.TIM3, &clocks);
         let mut write_timer = robot_core::get_counter(cx.device.TIM4, &clocks);
+        let mut tmc2209_communicator_timer = robot_core::get_counter(cx.device.TIM2, &clocks);
 
         read_timer.start(1.secs()).unwrap();
         write_timer.start(200.millis()).unwrap();
+        let tmc2209_timer_ticks = Duration::<u32, 1, TMC2209COMMUNICATOR_CLOCK_FREQ>::from_ticks(BIT_SEND_TICKS);
+        tmc2209_communicator_timer.start(tmc2209_timer_ticks).unwrap();
+        defmt::debug!("tmc2209_communicator_timer perior: {} nanos", tmc2209_timer_ticks.to_nanos());
 
         read_timer.listen(Event::Update);
         write_timer.listen(Event::Update);
+        tmc2209_communicator_timer.listen(Event::Update);
 
-        // TMC2209
-        let en = gpioc.pc7.into_push_pull_output(&mut gpioc.crl);
-        let x_stepper_uart_pin = gpioc.pc10.into_dynamic(&mut gpioc.crh);
-        let tmc2209_0 = Tmc2209Constructor::new(
-            en,
-            x_stepper_uart_pin,
-            cx.device.TIM2,
-            &clocks,
-            cx.local.tmc2209_req_ch_0,
-            cx.local.tmc2209_rsp_ch_0,
-        );
+        let (tmc2209_msg_sender, tmc2209_msg_receiver) = make_channel!(robot_core::my_tmc2209::Request, CHANNEL_CAPACITY);
+        let (tmc2209_rsp_sender, tmc2209_rsp_receiver) = make_channel!(u32, CHANNEL_CAPACITY);
 
+        let communicator = TMC2209SerialCommunicator::new(tmc2209_msg_receiver, tmc2209_rsp_sender, x_stepper_uart_pin);
         (
             Shared { x: 0 },
             Local {
-                communicator: tmc2209_0.communicator,
-                tmc2209_communicator_timer: tmc2209_0.timer,
-                tmc2209_msg_sender_1: tmc2209_0.req_sender.clone(),
-                tmc2209_msg_sender_2: tmc2209_0.req_sender,
+                communicator,
+                tmc2209_communicator_timer,
+                tmc2209_msg_sender_1: tmc2209_msg_sender.clone(),
+                tmc2209_msg_sender_2: tmc2209_msg_sender,
+                tmc2209_rsp_receiver,
 
                 read_timer,
                 write_timer,
@@ -152,36 +154,32 @@ mod app {
         cx.local.communicator.handle_interrupt();
     }
 
-    //INFO: I used this code to test communication with tmc2209. Now it is tested and works fine.
-    //So I moved 'tmc2209_rsp_receiver' inside Configurator, it is not clonable, so cant move it
-    //here.
-    //
-    // #[task(binds = TIM3, priority = 1, local = [read_timer, tmc2209_msg_sender_2, tmc2209_rsp_receiver, first: bool = true], shared = [x])]
-    // fn read_task(mut cx: read_task::Context) {
-    //     cx.local.read_timer.clear_interrupt(Event::Update);
+    #[task(binds = TIM3, priority = 1, local = [read_timer, tmc2209_msg_sender_2, tmc2209_rsp_receiver, first: bool = true], shared = [x])]
+    fn read_task(mut cx: read_task::Context) {
+        cx.local.read_timer.clear_interrupt(Event::Update);
 
-    //     if *cx.local.first {
-    //         *cx.local.first = false;
+        if *cx.local.first {
+            *cx.local.first = false;
 
-    //         let write_req = tmc2209::write_request(0, tmc2209::reg::SLAVECONF(0));
-    //         let req = robot_core::my_tmc2209::Request::write(write_req);
-    //         let _ = cx.local.tmc2209_msg_sender_2.try_send(req).ok();
-    //     } else {
-    //         match cx.local.tmc2209_rsp_receiver.try_recv() {
-    //             Err(_) => {}
-    //             Ok(response) => {
-    //                 let ifcnt_resp = tmc2209::reg::IFCNT::from(response);
-    //                 defmt::info!("Transmission counter: {}", ifcnt_resp.0);
-    //             } // Err(err) => defmt::warn!("Fail to receive response from communicator"),
-    //         }
+            let write_req = tmc2209::write_request(0, tmc2209::reg::SLAVECONF(0));
+            let req = robot_core::my_tmc2209::Request::write(write_req);
+            let _ = cx.local.tmc2209_msg_sender_2.try_send(req).ok();
+        } else {
+            match cx.local.tmc2209_rsp_receiver.try_recv() {
+                Err(_) => {}
+                Ok(response) => {
+                    let ifcnt_resp = tmc2209::reg::IFCNT::from(response);
+                    defmt::info!("Transmission counter: {}", ifcnt_resp.0);
+                } // Err(err) => defmt::warn!("Fail to receive response from communicator"),
+            }
 
-    //         let read_req = tmc2209::read_request::<tmc2209::reg::IFCNT>(0);
-    //         let req = robot_core::my_tmc2209::Request::read(read_req);
-    //         let res = cx.local.tmc2209_msg_sender_2.try_send(req).ok();
-    //         // match res {
-    //         //     None => defmt::debug!("fail to send message to Communicator"),
-    //         //     Some(_) => defmt::debug!("Successfully send READ message to Communicator"),
-    //         // }
-    //     }
-    // }
+            let read_req = tmc2209::read_request::<tmc2209::reg::IFCNT>(0);
+            let req = robot_core::my_tmc2209::Request::read(read_req);
+            let res = cx.local.tmc2209_msg_sender_2.try_send(req).ok();
+            // match res {
+            //     None => defmt::debug!("fail to send message to Communicator"),
+            //     Some(_) => defmt::debug!("Successfully send READ message to Communicator"),
+            // }
+        }
+    }
 }
